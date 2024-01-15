9e1b9e873da2b2a9ddd336f256752e3b07172ece
thiag-off <thiagoforto20@gmail.com>
2024-01-15 19:21:44 -0300

    DEBUG AND ALLOWED_HOSTS added to env

--- my_site/settings.py
+++ my_site/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
