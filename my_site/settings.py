c5779ab1af7b4afa06b61b658f1ab0fb7f6e8e32
thiag-off <thiagoforto20@gmail.com>
2024-01-15 16:35:17 -0300

    .env added

--- my_site/settings.py
+++ my_site/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-66%b!x$(c3v+(xos^+74qc_b+)+)cpz%978h(et9icausipbz#'
+
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -48,7 +53,7 @@ CHANNEL_LAYERS = {
     "default" : {
         "BACKEND" : "channels_redis.core.RedisChannelLayer",
         "CONFIG":{
-            'hosts':["rediss://red-cmgr2eta73kc73dshbug:mrWbsavRu7k2ya4j6Njb9xtiAbdd81s7@frankfurt-redis.render.com:6379"],
+            'hosts':[str(os.getenv('REDIS_KEY'))],
         },
     },
 }
